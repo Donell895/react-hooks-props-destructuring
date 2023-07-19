deab8cc9ac7e12f353f788fc79e56c0d917da30a
donell <donell.wambua@student.moringaschool.com>
2023-07-19 12:39:49 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,15 @@ import React from "react";
 import MovieCard from "./MovieCard";
 
 function App() {
-  const title = "Mad Max";
-  const posterURL =
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM";
-  const genresArr = ["Action", "Adventure", "Science Fiction", "Thriller"];
+  const movie = {
+    title: "Mad Max",
+    posterURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn1OTYGz2GDC1XjA9tirh_1Rd571yE5UFIYsmZp4nACMd7CCHM",
+    genres: ["Action", "Adventure", "Science Fiction", "Thriller"]
+  };
 
   return (
     <div className="App">
-      <MovieCard title={title} genres={genresArr} />
+      <MovieCard {...movie} />
     </div>
   );
 }
